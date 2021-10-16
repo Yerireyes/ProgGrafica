b22a584cde49828a4d7239231769cc12ac93be44
Yerireyes <le_soleto@hotmail.com>
2021-10-15 23:36:48 -0400

    Cambiando WriteLine

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello down");
+            Console.WriteLine("Impacto de Genchi");
             Game game = new Game(1000, 700, ":D");
             game.Run();
         }
